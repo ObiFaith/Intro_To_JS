ea10f18f0a9f04b0b6d5632f1e1466675e8f8a8c
Obi Faith <obifth@gmail.com>
2022-06-21 16:14:42 +0100

    Intro_To_JS
    JavaScript completing the content in your HTML Elements
    
    Assign values to three different variables and output them in the HTML Documet

---
+++ About_Me__JS/script.js
@@ -0,0 +1,7 @@
+var name = 'Obi Faith';
+var country = 'Nigeria';
+var height = '160cm';
+
+document.getElementById('name').innerHTML = name;
+document.getElementById('country').innerHTML = country;
+document.getElementById('height').innerHTML = height;
